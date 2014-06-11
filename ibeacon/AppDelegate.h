56215921c4249ea5834791fcd09b2ca64cee30b0
Alejandro Ramos Grifé <Alex@MacBook-Pro-de-Alejandro-2.local>
2014-06-11 22:20:10 +0200

    Initial Commit

---
+++ ibeacon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ibeacon
+//
+//  Created by Alejandro Ramos Grifé on 11/06/14.
+//  Copyright (c) 2014 innovalonia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
